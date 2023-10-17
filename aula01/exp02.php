577f26cdcff1bdedeadac18db2f79aaf88a09d31
gabriel-nestor17 <145491996+gabriel-nestor17@users.noreply.github.com>
2023-10-17 16:26:00 -0300

    Add files via upload

---
+++ aula01/exp02.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <?php
+    $color ="branco"; 
+    echo "a cor e ".$color."<hr>";
+    ?> 
+</body>
+</html>
---
+++ aula01/exp04.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$color ="preto";
+$x = 23;
+$r = 12.3;
+$resul = $x + $r;
+echo $resul; 
+?>   
+</body>
+</html>
